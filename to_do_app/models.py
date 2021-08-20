1c92928c4318df3a3c6616637454546d12ac249f
Sudo-Milin <milin.git@git.com>
2021-08-20 18:07:19 +0530

    ok from admin

--- to_do_app/models.py
+++ to_do_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+    class Meta():
+        verbose_name_plural = "List"
+    task = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from to_do_app.models import List
+from typing import ContextManager
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def ToDoList(request):
+    list_of_task = List.objects.all()
+    context =  {
+            "list_of_task" : list_of_task
+    }
+    return render(request, "to_do_app/home.html", context)
