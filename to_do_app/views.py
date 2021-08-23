68fdbe8b4b44a355e346786341958d73fd0e7b88
Sudo-Milin <milin.git@git.com>
2021-08-23 12:32:25 +0530

    template remains

--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,28 +1,22 @@
-from to_do_app.models import List
-from typing import ContextManager
 from django.shortcuts import render, redirect
+from . models import List
 from . forms import ListModelForm
 # Create your views here.
 
 def ToDoList(request):
-    list_of_task = List.objects.all()
-    context =  {
-            "list_of_task" : list_of_task
-    }
-    return render(request, "to_do_app/home.html", context)
-
-def add_task(request):
+    list_of_task = List.objects.all().order_by("-date_added")
     form = ListModelForm()
     if request.method == "POST":
         form = ListModelForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("/")
-    context = {
-        "form": form
+
+    context =  {
+            "list_of_task" : list_of_task,
+            "form": form
     }
     return render(request, "to_do_app/home.html", context)
-
 
 def delete_task(request, pk):
         task = List.objects.get(id=pk)
